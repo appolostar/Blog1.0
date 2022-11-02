c6140e99eec59c82a6fe04e5ee42574b2c145ccb
ShawayL <741077606@qq.com>
2022-11-02 23:21:50 +0800

    移动文章目录到左上角，修改右侧栏只在主页才显示

--- themes/icarus/layout/layout.jsx
+++ themes/icarus/layout/layout.jsx
@@ -14,7 +14,37 @@ module.exports = class extends Component {
         const language = page.lang || page.language || config.language;
         const columnCount = Widgets.getColumnCount(config.widgets, config, page);
 
-        return <html lang={language ? language.substr(0, 2) : ''}>
+        if(page.path==='index.html')
+        {
+            return <html lang={language ? language.substr(0, 2) : ''}>
+                <Head site={site} config={config} helper={helper} page={page} />
+                <body class={`is-${columnCount}-column`}>
+                    <Navbar config={config} helper={helper} page={page} />
+                    <section class="section">
+                        <div class="container">
+                            <div class="columns">
+                                <div class={classname({
+                                    column: true,
+                                    'order-2': true,
+                                    'column-main': true,
+                                    'is-12': columnCount === 1,
+                                    'is-8-tablet is-8-desktop is-8-widescreen': columnCount === 2,
+                                    'is-8-tablet is-8-desktop is-6-widescreen': columnCount === 3
+                                })} dangerouslySetInnerHTML={{ __html: body }}></div>
+                                <Widgets site={site} config={config} helper={helper} page={page} position={'left'} />
+                                <Widgets site={site} config={config} helper={helper} page={page} position={'right'} />
+                            </div>
+                        </div>
+                    </section>
+                    <Footer config={config} helper={helper} />
+                    <Scripts site={site} config={config} helper={helper} page={page} />
+                    <Search config={config} helper={helper} />
+                </body>
+            </html>;
+        }
+        else
+        {
+            return <html lang={language ? language.substr(0, 2) : ''}>
             <Head site={site} config={config} helper={helper} page={page} />
             <body class={`is-${columnCount}-column`}>
                 <Navbar config={config} helper={helper} page={page} />
@@ -27,10 +57,9 @@ module.exports = class extends Component {
                                 'column-main': true,
                                 'is-12': columnCount === 1,
                                 'is-8-tablet is-8-desktop is-8-widescreen': columnCount === 2,
-                                'is-8-tablet is-8-desktop is-6-widescreen': columnCount === 3
+                                'is-8-tablet is-8-desktop is-9-widescreen': columnCount === 3
                             })} dangerouslySetInnerHTML={{ __html: body }}></div>
                             <Widgets site={site} config={config} helper={helper} page={page} position={'left'} />
-                            <Widgets site={site} config={config} helper={helper} page={page} position={'right'} />
                         </div>
                     </div>
                 </section>
@@ -39,5 +68,8 @@ module.exports = class extends Component {
                 <Search config={config} helper={helper} />
             </body>
         </html>;
+        }
+
+
     }
 };
